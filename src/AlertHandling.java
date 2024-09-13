4e29857ec7d08aba827f093eaa4d5f468129453d
GauravGidye26 <gauravgidye26@gmail.com>
2024-09-13 16:38:20 +0530

    Added HTML Reports with Screenshots

--- src/AlertHandling.java
+++ src/AlertHandling.java
@@ -1,45 +1,88 @@
-import java.time.Duration;
-
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
 
 public class AlertHandling {
 
-    public static void main(String[] args) {
-    	System.setProperty("webdriver.chrome.driver", "C:/Users/gauravg/Documents/chromedriver.exe");
+    private WebDriver driver;
+    private ExtentReports extent;
+    private ExtentTest test;
+    private WebDriverWait wait;
 
-        WebDriver driver = new ChromeDriver();
+    @BeforeSuite
+    public void setUp() {
+        ExtentSparkReporter spark = new ExtentSparkReporter("HTML_Reports/AlertHandling.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/gauravg/Documents/chromedriver.exe");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); 
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        test = extent.createTest("Alert Handling Test").assignAuthor("Gaurav").assignCategory("Functional Test Case").assignDevice("Windows");
+        test.info("Browser launched and maximized");
+    }
 
+    @Test
+    public void handleAlertTest() throws IOException {
         try {
-        	driver.get("https://letcode.in/waits");
+            driver.get("https://letcode.in/waits");
+            test.info("Navigated to the waits page");
 
-        	WebElement simpleAlertButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Simple Alert']")));
-            
-//        	WebElement simpleAlertButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[text()='Simple Alert']")));
-
-//        	WebElement simpleAlertButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Simple Alert']")));
-
+            WebElement simpleAlertButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Simple Alert']")));
             simpleAlertButton.click();
+            test.info("Clicked on Simple Alert button");
 
             wait.until(ExpectedConditions.alertIsPresent());
-
             Alert alert = driver.switchTo().alert();
             
+            test.info("Alert is present");
+
             Thread.sleep(2000);
 
             alert.accept();
+            test.pass("Alert accepted successfully");
+
+            test.addScreenCaptureFromPath(captureScreenshot(driver));
+
         } catch (Exception e) {
+            test.fail("Exception occurred: " + e.getMessage());
             e.printStackTrace();
-        } finally {
-            driver.quit();
         }
     }
+
+    @AfterSuite
+    public void tearDown() {
+        driver.quit();
+        test.info("Browser closed");
+
+        extent.flush();
+    }
+
+    private String captureScreenshot(WebDriver driver) throws IOException {
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File destFilePath = new File("src/../Images/Screenshot" + System.currentTimeMillis() + ".png");
+        String absolutePathLocation = destFilePath.getAbsolutePath();
+        FileUtils.copyFile(srcFile, destFilePath);
+        return absolutePathLocation;
+    }
 }
+
--- src/FileHandling.java
+++ src/FileHandling.java
@@ -1,59 +1,97 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
 
 public class FileHandling {
 
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/gauravg/Documents/chromedriver.exe");
+    private WebDriver driver;
+    private ExtentReports extent;
+    private ExtentTest test;
 
-        WebDriver driver = new ChromeDriver();
+    @BeforeSuite
+    public void setUp() {
+        ExtentSparkReporter spark = new ExtentSparkReporter("HTML_Reports/FileHandling.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/gauravg/Documents/chromedriver.exe");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        driver.get("https://letcode.in/file");
-        
+        test = extent.createTest("File Handling Test").assignAuthor("Gaurav").assignCategory("Functional Test Case").assignDevice("Windows");
+        test.info("Browser launched and maximized");
+    }
+
+    @Test
+    public void fileHandlingTest() throws IOException {
         String uploadFileName = "TestFile.txt";
-        String uploadFilePath = "C:\\Users\\gauravg\\eclipse-workspace\\FileAndDynamicEleHandling\\SampleFile\\"+uploadFileName;
+        String uploadFilePath = "C:\\Users\\gauravg\\eclipse-workspace\\FileAndDynamicEleHandling\\SampleFile\\" + uploadFileName;
         String downloadDir = "C:\\Users\\gauravg\\Downloads";
         String expectedDownloadFileName = "sample.txt";
 
         try {
+            driver.get("https://letcode.in/file");
+            test.info("Navigated to the file handling page");
+
             WebElement uploadElement = driver.findElement(By.cssSelector("input[type='file']"));
             uploadElement.sendKeys(uploadFilePath);
+            test.info("Uploaded file: " + uploadFileName);
 
             String uploadedFileName = uploadElement.getAttribute("value");
-//          System.out.println(uploadedFileName);
             if (uploadedFileName != null && uploadedFileName.contains(uploadFileName)) {
-                System.out.println("File uploaded successfully!");
+                test.pass("File uploaded successfully!");
             } else {
                 throw new Exception("File upload failed: Uploaded file not found in the input field.");
             }
-        } catch (Exception e) {
-            System.err.println("Exception during file upload: " + e.getMessage());
-        }
 
-        try {
-            WebElement downloadButton = driver.findElement(By.xpath("//button//a[@download='"+expectedDownloadFileName+"']"));
+            WebElement downloadButton = driver.findElement(By.xpath("//button//a[@download='" + expectedDownloadFileName + "']"));
             downloadButton.click();
-            
+            test.info("Clicked download button for file: " + expectedDownloadFileName);
+
             Thread.sleep(5000);  
 
             File downloadFile = Paths.get(downloadDir, expectedDownloadFileName).toFile();
             if (downloadFile.exists()) {
-                System.out.println("File downloaded successfully!");
+                test.pass("File downloaded successfully!");
             } else {
                 throw new Exception("File download failed: File not found in the download directory.");
             }
-        } catch (InterruptedException e) {
-            System.err.println("Thread interrupted: " + e.getMessage());
+
+            test.addScreenCaptureFromPath(captureScreenshot(driver));
+
         } catch (Exception e) {
-            System.err.println("Exception during file download: " + e.getMessage());
-        } finally {
-            driver.quit();
+            test.fail("Exception occurred: " + e.getMessage());
+            e.printStackTrace();
         }
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        driver.quit();
+        test.info("Browser closed");
+
+        extent.flush();
+    }
+
+    private String captureScreenshot(WebDriver driver) throws IOException {
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File destFilePath = new File("src/../Images/Screenshot" + System.currentTimeMillis() + ".png");
+        String absolutePathLocation = destFilePath.getAbsolutePath();
+        FileUtils.copyFile(srcFile, destFilePath);
+        return absolutePathLocation;
     }
 }
